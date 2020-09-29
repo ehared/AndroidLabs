101d6235a906a38cfeb729dbe6ef3859db1a827f
ehared <eltire.hared@hotmail.com>
2020-09-29 03:08:11 -0400

    step 3 - initial commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
 
     }
