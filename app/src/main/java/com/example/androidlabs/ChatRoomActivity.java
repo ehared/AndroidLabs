ec8e4592a9e196fdd97ba9f3e67ea4d9fa5402c9
ehared <eltire.hared@hotmail.com>
2020-10-16 00:02:42 -0400

    Lab 4 finished

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -109,4 +109,29 @@ public class ChatRoomActivity extends AppCompatActivity {
             return newView;
         }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
